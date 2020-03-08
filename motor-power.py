31ab1de3ce1f226a38f245a8f2d664679587dec9
Chris Aung <55330129+chrrisa18@users.noreply.github.com>
2020-03-07 21:10:23 -0700

    Add files via upload

---
+++ motor-power.py
@@ -0,0 +1,20 @@
+#power calculations for motor 
+
+import numpy as np
+import math
+import sys
+
+e_mc = 0.7 #motor controller efficiency, subject to 
+e_m = 0.7 #motor efficiency, subject to change
+
+DC_voltage = 0 #dc voltage in v
+DC_current = 0 #dc current in A
+
+def calculate_pout(dc_v, dc_c):
+    power_in = dc_v * dc_c 
+    power_controller = power_in * e_mc
+    #alternatively, power_controller = sqrt(3) / 2 * Vrms * Irms
+    power_out = power_controller * e_m
+    #alternatively, power_out = torque * Revolutions/min = Force* V_car 
+    #torque = rwheel * Forcewheel, RPM = V/rwheel
+    return power_out 
