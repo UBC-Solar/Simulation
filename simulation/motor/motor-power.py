247b2770cd875111986603da1ed65a252c475e82
chrrisa18 <azchris00@gmail.com>
2020-03-21 11:55:46 -0600

    added update function

--- simulation/motor/motor-power.py
+++ simulation/motor/motor-power.py
@@ -1,20 +1,25 @@
-#power calculations for motor 
+from abc import ABC, abstractmethod
 
-import numpy as np
-import math
-import sys
+class Motor_Power(BaseMotor):
+    #power calculations for motor 
 
-e_mc = 0.7 #motor controller efficiency, subject to 
-e_m = 0.7 #motor efficiency, subject to change
+    import numpy as np
+    import math
+    import sys
 
-DC_voltage = 0 #dc voltage in v
-DC_current = 0 #dc current in A
+    e_mc = 0.7 #motor controller efficiency, subject to 
+    e_m = 0.7 #motor efficiency, subject to change
 
-def calculate_pout(dc_v, dc_c):
-    power_in = dc_v * dc_c 
-    power_controller = power_in * e_mc
-    #alternatively, power_controller = sqrt(3) / 2 * Vrms * Irms
-    power_out = power_controller * e_m
-    #alternatively, power_out = torque * Revolutions/min = Force* V_car 
-    #torque = rwheel * Forcewheel, RPM = V/rwheel
-    return power_out 
+    DC_voltage = 0 #dc voltage in v
+    DC_current = 0 #dc current in A
+
+    def calculate_pout(dc_v, dc_c):
+        power_in = dc_v * dc_c 
+        power_controller = power_in * e_mc
+        #alternatively, power_controller = sqrt(3) / 2 * Vrms * Irms
+        power_out = power_controller * e_m
+        #alternatively, power_out = torque * Revolutions/min = Force* V_car 
+        #torque = rwheel * Forcewheel, RPM = V/rwheel
+        return power_out 
+    
+    def update(self,tick) 
