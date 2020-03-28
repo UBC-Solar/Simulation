f14f39d90f1e6126263a2679e13c54ba16f23865
fisherxue <fisherxue0618@gmail.com>
2020-03-28 13:53:41 -0700

    fixed style issues

--- simulation/regen/base_regen.py
+++ simulation/regen/base_regen.py
@@ -4,6 +4,3 @@ class BaseRegen(Producer):
     def __init__(self):
         super().__init__(self)
 
-    def update(self):
-
-        return 0
---
+++ simulation/regen/basic_regen.py
@@ -0,0 +1,9 @@
+from simulation.regen.base_regen import BaseRegen
+
+class BasicRegen(BaseRegen):
+    def __init__(self):
+        super().__init__(self)
+
+    def update(self, tick):
+        self.produced_energy = 0
+        return self.produced_energy * tick
