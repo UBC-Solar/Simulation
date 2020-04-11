c0d0513f8c23ad57f915a47d5b131f4a1fa42155
Mihir Nimgade <mihir.nim@outlook.com>
2020-04-11 13:25:42 -0700

    Changed BasicBattery __init__() implementation and integrated update() method into charge() and discharge()

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -2,14 +2,15 @@ from simulation.common import Consumer
 from simulation.common import Producer
 from simulation.common import Storage
 
-from simulation.array import BaseArray
+# from simulation.array import BaseArray
 from simulation.battery import BaseBattery
-from simulation.lvs import BaseLVS
-from simulation.motor import BaseMotor
-from simulation.regen import BaseRegen
+# from simulation.lvs import BaseLVS
+# from simulation.motor import BaseMotor
+# from simulation.regen import BaseRegen
 
-from simulation.array import BasicArray
+# from simulation.array import BasicArray
 
 from simulation.battery import BasicBattery
 
 __version__ = "0.0.1"
+
--- simulation/battery/basic_battery.py
+++ simulation/battery/basic_battery.py
@@ -2,15 +2,23 @@ from simulation.battery.base_battery import BaseBattery
 
 
 class BasicBattery(BaseBattery):
-    def __init__(self, initial_energy, max_current_capacity, max_energy_capacity):
-        super().__init__(initial_energy, max_current_capacity, max_energy_capacity)    
+    def __init__(self, state_of_charge):
 
-        self.state_of_charge = 1
-        self.depth_of_discharge = 1 - self.state_of_charge
-        self.voltage = self.calculate_voltage(discharge_capacity=0)
-
+        # DayBreak battery constants
         self.max_voltage = 117.6
         self.min_voltage = 70
+        self.max_energy_capacity = 4573         # in Wh
+        self.max_current_capacity = 48.75       # in Ah
+
+        self.state_of_charge = state_of_charge
+        self.depth_of_discharge = 1 - self.state_of_charge
+
+        self.discharge_capacity = self.calculate_discharge_capacity_from_soc(self.state_of_charge)      # calculates discharge capacity from state of charge
+        self.voltage = self.calculate_voltage(self.discharge_capacity)               # calculates voltage of battery from discharge capacity
+        self.stored_energy = self.max_energy_capacity - self.calculate_energy_discharged_from_discharge_capacity(self.discharge_capacity)       # calculates stored energy from discharge capacity
+
+        super().__init__(self.stored_energy, self.max_current_capacity, self.max_energy_capacity, self.max_voltage,
+                         self.min_voltage, self.voltage, self.state_of_charge)
 
     @staticmethod
     def calculate_discharge_capacity(energy):
@@ -22,12 +30,27 @@ class BasicBattery(BaseBattery):
 
     @staticmethod
     def calculate_voltage(discharge_capacity):
-        return 117.6 - 0.488 * discharge_capacity
+        return 117.6 - 0.97641 * discharge_capacity
 
-    # TODO: make it so that I don't need to call self.update() after calling self.discharge() every time
+    @staticmethod
+    def calculate_discharge_capacity_from_soc(state_of_charge):
+        return 48.75 * (1 - state_of_charge)
+
+    @staticmethod
+    def calculate_energy_discharged_from_discharge_capacity(discharge_capacity):
+        return 117.6 * discharge_capacity - 0.488 * pow(discharge_capacity, 2)
 
     def update(self, tick):     # updates relevant battery variables
         discharge_capacity = self.calculate_discharge_capacity(self.max_energy_capacity - self.stored_energy)
         self.state_of_charge = self.calculate_state_of_charge(discharge_capacity)
         self.voltage = self.calculate_voltage(discharge_capacity)
+
+    def charge(self, energy):
+        super().charge(energy)
+        self.update(1)                  # this makes it so that the update() method need not always be called separately
+
+    def discharge(self, energy):
+        super().discharge(energy)
+        self.update(1)
+
 
