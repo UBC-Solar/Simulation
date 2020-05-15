0c4ce7b1a55624e9af2ce2711815356cbd9c17b2
Mihir Nimgade <mihir.nim@outlook.com>
2020-05-14 19:38:13 -0700

    Style changes

--- simulation/battery/basic_battery.py
+++ simulation/battery/basic_battery.py
@@ -1,21 +1,27 @@
 from simulation.battery.base_battery import BaseBattery
 
+
 class BasicBattery(BaseBattery):
 
     def __init__(self, state_of_charge):
-
         # DayBreak battery constants
         self.max_voltage = 117.6
         self.min_voltage = 70
-        self.max_energy_capacity = 4573         # in Wh
-        self.max_current_capacity = 48.75       # in Ah
+        self.max_energy_capacity = 4573  # in Wh
+        self.max_current_capacity = 48.75  # in Ah
 
-        self.state_of_charge = state_of_charge                      # 0.00 <= state_of_charge <= 1.00
+        self.state_of_charge = state_of_charge  # 0.00 <= state_of_charge <= 1.00
         self.depth_of_discharge = 1 - self.state_of_charge
 
-        self.discharge_capacity = self.calculate_discharge_capacity_from_soc(self.state_of_charge)                                              # calculates discharge capacity from state of charge
-        self.voltage = self.calculate_voltage(self.discharge_capacity)                                                                          # calculates voltage of battery from discharge capacity
-        self.stored_energy = self.max_energy_capacity - self.calculate_energy_discharged_from_discharge_capacity(self.discharge_capacity)       # calculates stored energy from discharge capacity
+        # calculates discharge capacity from state of charge
+        self.discharge_capacity = self.calculate_discharge_capacity_from_soc(self.state_of_charge)
+
+        # calculates voltage of battery from discharge capacity
+        self.voltage = self.calculate_voltage(self.discharge_capacity)
+
+        # calculates stored energy from discharge capacity
+        self.stored_energy = self.max_energy_capacity - self.calculate_energy_discharged_from_discharge_capacity(
+            self.discharge_capacity)
 
         super().__init__(self.stored_energy, self.max_current_capacity, self.max_energy_capacity, self.max_voltage,
                          self.min_voltage, self.voltage, self.state_of_charge)
@@ -40,18 +46,16 @@ class BasicBattery(BaseBattery):
     def calculate_energy_discharged_from_discharge_capacity(discharge_capacity):
         return 117.6 * discharge_capacity - 0.488 * pow(discharge_capacity, 2)
 
-    def update(self, tick):     # updates relevant battery variables
+    def update(self, tick):  # updates relevant battery variables
         discharge_capacity = self.calculate_discharge_capacity(self.max_energy_capacity - self.stored_energy)
         self.state_of_charge = self.calculate_state_of_charge(discharge_capacity)
         self.voltage = self.calculate_voltage(discharge_capacity)
 
-    def charge(self, energy):           # assuming energy parameter is in joules
-        super().charge(energy / 3600)   # divide by 3600 to convert from joules to watt-hours
-        #self.update(1)                  # this makes it so that the update() method need not always be called separately
+    # assuming energy parameter is in joules
+    def charge(self, energy):
+        super().charge(energy / 3600)  # divide by 3600 to convert from joules to watt-hours
+        # self.update(1)               # this makes it so that the update() method need not always be called separately
 
     def discharge(self, energy):
         super().discharge(energy / 3600)
-        #self.update(1)
-
-
-# TODO: create class for energy transfer between components maybe?
+        # self.update(1)
