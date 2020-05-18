636d275a3f18083aedfa26551a3b639d7c9ebb4c
Mihir Nimgade <mihir.nim@outlook.com>
2020-05-18 03:01:29 -0700

    Added __str__() method

--- simulation/array/basic_array.py
+++ simulation/array/basic_array.py
@@ -1,18 +1,15 @@
 from simulation.array.base_array import BaseArray
 
+
 class BasicArray(BaseArray):
 
-    #incident_sunlight: 
+    # incident_sunlight:
     def __init__(self, incident_sunlight):
         super().__init__()
 
         self.sunlight = incident_sunlight
         self.panel_efficiency = 0.2
         self.panel_size = 6
-
-        print("BasicArray: incident_sunlight:  {} W/m2".format(self.sunlight))
-        print("BasicArray: panel_size: {} m2".format(self.panel_size))
-        print("BasicArray: panel_efficiency: {}%".format(self.panel_efficiency*100))
 
     @staticmethod
     def calculate_produced_power(sunlight, panel_efficiency, panel_size):
@@ -27,9 +24,11 @@ class BasicArray(BaseArray):
 
         """
 
-        #Assume constant sunlight in this simple model.
-        self.produced_energy = self.calculate_produced_power(self.sunlight, \
-                                         self.panel_efficiency, self.panel_size) * tick
+        # Assume constant sunlight in this simple model.
+        self.produced_energy = self.calculate_produced_power(self.sunlight,
+                                                             self.panel_efficiency, self.panel_size) * tick
 
-
-
+    def __str__(self):
+        return(f"BasicArray: incident_sunlight: {self.sunlight}W/m2\n" +
+               f"BasicArray: panel_size: {self.panel_size}m2\n" +
+               f"BasicArray: panel_efficiency: {self.panel_efficiency * 100}%\n")
--- simulation/motor/basic_motor.py
+++ simulation/motor/basic_motor.py
@@ -1,15 +1,15 @@
 from simulation.motor.base_motor import BaseMotor
 
+
 class BasicMotor(BaseMotor):
 
     def __init__(self):
-
         super().__init__()
 
-        #Instantaneous voltage supplied by the battery to the motor controller
+        # Instantaneous voltage supplied by the battery to the motor controller
         self.dc_v = 0
-        
-        #Instantaneous current supplied by the battery to the motor controller
+
+        # Instantaneous current supplied by the battery to the motor controller
         self.dc_i = 0
 
         self.input_power = 0
@@ -24,54 +24,52 @@ class BasicMotor(BaseMotor):
 
         self.friction_force = (self.vehicle_mass * self.acceleration_g * self.road_friction)
 
-        self.e_mc = 0.98 #motor controller efficiency, subject to change
-        self.e_m = 0.9  #motor efficiency, subject to change
+        self.e_mc = 0.98  # motor controller efficiency, subject to change
+        self.e_m = 0.9  # motor efficiency, subject to change
 
-        #print("torque experienced by motor: {} Nm".format(self.constant_torque))
-        print("tire radius: {}m".format(self.tire_radius))
-        print("rolling resistance coefficient: {}".format(self.road_friction))
-        print("vehicle mass: {}kg".format(self.vehicle_mass))
-        print("acceleration of gravity: {}m/s2".format(self.acceleration_g))
-        print("motor controller efficiency: {}%".format(self.e_mc))
-        print("motor efficiency: {}%".format(self.e_m))
-   
-    #Calculates the power transferred to the wheel by the motor and the motor controller 
+        # print("torque experienced by motor: {} Nm".format(self.constant_torque))
+
+    # Calculates the power transferred to the wheel by the motor and the motor controller
     def calculate_power_out(self):
-
         power_in = self.dc_v * self.dc_i
         power_controller = power_in * self.e_mc
 
-        #alternatively, power_controller = sqrt(3) / 2 * Vrms * Irms
+        # alternatively, power_controller = sqrt(3) / 2 * Vrms * Irms
         power_out = power_controller * self.e_m
 
-        #alternatively, power_out = torque * Revolutions/min = Force* V_car 
-        #torque = rwheel * Forcewheel, RPM = V/rwheel
+        # alternatively, power_out = torque * Revolutions/min = Force* V_car
+        # torque = rwheel * Forcewheel, RPM = V/rwheel
 
         return power_out
 
-    #For the motor, the energy consumed by the motor/motor controller depends on the voltage and
+    # For the motor, the energy consumed by the motor/motor controller depends on the voltage and
     #   current supplied by the battery to the motor controller
     def update_motor_input(self, dc_v, dc_i):
-    
         self.dc_v = dc_v
         self.dc_i = dc_i
-    
+
     def calculate_power_in(self, required_speed_kmh):
-
         required_speed_ms = required_speed_kmh / 3.6
-        required_angular_speed_rads = required_speed_ms / self.tire_radius 
+        required_angular_speed_rads = required_speed_ms / self.tire_radius
 
-        drag_force = 0.5 * self.air_density * ((required_speed_ms)**2) *\
-                     self.drag_coefficient * self.vehicle_frontal_area
+        drag_force = 0.5 * self.air_density * (
+                    required_speed_ms ** 2) * self.drag_coefficient * self.vehicle_frontal_area
 
         motor_output_power = required_angular_speed_rads * (self.friction_force + drag_force)
 
-        motor_input_power = motor_output_power/self.e_m
-        
+        motor_input_power = motor_output_power / self.e_m
+
         self.input_power = motor_input_power / self.e_mc
-      
-    def update(self, tick):         
-        #For the motor, the update tick calculates a value for the energy expended in a period of
-        #   time
+
+    def update(self, tick):
+        # For the motor, the update tick calculates a value for the energy expended in a period of time
 
         self.consumed_energy = self.input_power * tick
+
+    def __str__(self):
+        return(f"Tire radius: {self.tire_radius}m"
+               f"Rolling resistance coefficient: {self.road_friction}"
+               f"Vehicle mass: {self.vehicle_mass}kg"
+               f"Acceleration of gravity: {self.acceleration_g}m/s2"
+               f"Motor controller efficiency: {self.e_mc}%"
+               f"Motor efficiency: {self.e_m}%")
