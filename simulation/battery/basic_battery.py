59c0145f29c667f5cef47582400adb93be0bcb38
Mihir Nimgade <mihir.nim@outlook.com>
2020-06-03 20:19:27 -0700

    Added support for NumPy array battery calculations

--- simulation/battery/basic_battery.py
+++ simulation/battery/basic_battery.py
@@ -1,5 +1,9 @@
-from simulation.battery.base_battery import BaseBattery
+import math
+
+import numpy as np
 from numpy.polynomial import Polynomial
+
+from simulation.battery.base_battery import BaseBattery
 
 
 class BasicBattery(BaseBattery):
@@ -35,13 +39,15 @@ class BasicBattery(BaseBattery):
 
         # ----- DayBreak battery equations -----
 
-        self.calculate_voltage_from_discharge_capacity = Polynomial([117.6, -0.858896])    # -0.97641x + 117.6
+        self.calculate_voltage_from_discharge_capacity = Polynomial([117.6, -0.858896])  # -0.8589x + 117.6
 
-        self.calculate_energy_from_discharge_capacity = Polynomial([0, 117.6, -0.429448])    # -0.488x^2 + 117.6x
+        self.calculate_energy_from_discharge_capacity = Polynomial([0, 117.6, -0.429448])  # -0.4294x^2 + 117.6x
 
         self.calculate_soc_from_discharge_capacity = Polynomial([1, -1 / self.max_current_capacity])
 
         self.calculate_discharge_capacity_from_soc = Polynomial([self.max_current_capacity, -self.max_current_capacity])
+
+        self.calculate_discharge_capacity_from_energy = lambda x: 136.92 - math.sqrt(18747.06027 - 2.32857*x)
 
         # ----- DayBreak battery variables -----
 
@@ -71,10 +77,10 @@ class BasicBattery(BaseBattery):
         """
 
         energy_discharged = self.max_energy_capacity - self.stored_energy
-        discharge_capacity = (self.calculate_energy_from_discharge_capacity - energy_discharged).roots()[0]
 
-        self.state_of_charge = self.calculate_soc_from_discharge_capacity(discharge_capacity)
-        self.voltage = self.calculate_voltage_from_discharge_capacity(discharge_capacity)
+        self.discharge_capacity = self.calculate_discharge_capacity_from_energy(energy_discharged)
+        self.state_of_charge = self.calculate_soc_from_discharge_capacity(self.discharge_capacity)
+        self.voltage = self.calculate_voltage_from_discharge_capacity(self.discharge_capacity)
 
     def charge(self, energy):
         """
@@ -94,3 +100,32 @@ class BasicBattery(BaseBattery):
         """
 
         super().discharge(energy / 3600)
+
+    def update_array(self, cumulative_energy_array):
+        """
+        Performs energy calculations with NumPy arrays
+
+        :param cumulative_energy_array: a NumPy array containing the cumulative energy changes at each time step
+        experienced by the battery
+
+        :return stored_energy_array: a NumPy array containing the energy stored in the battery at each time step
+
+        :return discharge_capacity_array: a NumPy array containing the discharge capacity of the battery at each time
+        step
+
+        :return soc_array: a NumPy array containing the battery state of charge at each time step
+        """
+
+        stored_energy_array = np.full(cumulative_energy_array.size, fill_value=self.stored_energy)
+        stored_energy_array += cumulative_energy_array / 3600
+        stored_energy_array = np.clip(stored_energy_array, a_min=0, a_max=self.max_energy_capacity)
+
+        energy_discharged_array = np.full(cumulative_energy_array.size, fill_value=self.max_energy_capacity) - \
+                                  stored_energy_array
+
+        discharge_capacity_array = np.array([self.calculate_discharge_capacity_from_energy(energy)
+                                             for energy in energy_discharged_array])
+
+        soc_array = self.calculate_soc_from_discharge_capacity(discharge_capacity_array)
+
+        return stored_energy_array, discharge_capacity_array, soc_array
