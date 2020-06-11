0137a79f8bb4199a3894a3fd37f57e6877ad7a93
Mihir Nimgade <mihir.nim@outlook.com>
2020-06-10 19:15:02 -0700

    Added documentation and switch to arange()

--- examples/max_distance_from_time_using_arrays.py
+++ examples/max_distance_from_time_using_arrays.py
@@ -33,12 +33,15 @@ basic_motor = simulation.BasicMotor()
 
 # ----- Array initialisation -----
 
-time = np.linspace(0, simulation_duration, num=int(simulation_duration / tick) + 1, dtype='f4')
+time = np.arange(0, simulation_duration + tick, tick, dtype='f4')
 
-speeds_simulated = np.linspace(1, max_speed, num=int(max_speed / speed_increment), dtype='f4')
+# stores the speeds that are considered in the simulation
+speeds_simulated = np.arange(1, max_speed + speed_increment, speed_increment, dtype='f4')
 
+# creates a 2D array of simulated speeds at each time step
 speed_kmh = np.meshgrid(time, speeds_simulated)[-1]
 
+# used to calculate car's time in motion
 tick_array = np.full_like(speed_kmh, fill_value=tick, dtype='f4')
 tick_array[:, 0] = 0
 
@@ -56,6 +59,7 @@ motor_consumed_energy = basic_motor.get_consumed_energy()
 produced_energy = basic_array.get_produced_energy()
 consumed_energy = motor_consumed_energy + lvs_consumed_energy
 
+# array that stores the energy transferred from/to battery for each simulated speed and each time step
 delta_energy = produced_energy - consumed_energy
 
 # ----- Array calculations -----
@@ -66,14 +70,19 @@ battery_variables_array = basic_battery.update_array(cumulative_delta_energy)
 
 state_of_charge = battery_variables_array[0].round(3) + 0.
 
+# when the battery SOC is empty, the car doesn't move
 speed_kmh = np.logical_and(speed_kmh, state_of_charge) * speed_kmh
+
 time_in_motion = np.logical_and(tick_array, state_of_charge) * tick
 
 time_taken = np.sum(time_in_motion, axis=1)
 
+# stores final SOC for each simulated speed
 final_soc = state_of_charge[:, -1] * 100 + 0.
 
-distance = speed_kmh * (tick_array / 3600)
+distance = speed_kmh * (time_in_motion / 3600)
+
+# array storing the total distance travelled at each speed
 distance_travelled = np.sum(distance, axis=1)
 
 # ----- Simulation output -----
@@ -81,11 +90,14 @@ distance_travelled = np.sum(distance, axis=1)
 max_distance = np.amax(distance_travelled)
 max_distance_index = np.argmax(distance_travelled)
 
+# speed that produces the maximum distance
+max_distance_speed = speeds_simulated[max_distance_index]
+
+# time taken to travel the maximum distance
 max_distance_time = time_taken[max_distance_index]
 max_distance_time = str(datetime.timedelta(seconds=int(max_distance_time)))
 
-max_distance_speed = speeds_simulated[max_distance_index]
-
+# final battery SOC when max distance is travelled
 max_distance_final_soc = final_soc[max_distance_index]
 
 print(f"Simulation complete!\n\n"
