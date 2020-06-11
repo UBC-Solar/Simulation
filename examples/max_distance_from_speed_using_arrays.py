Fixed off-by-one error and cleaned code

examples/max_distance_from_speed_using_arrays.py
@@ -52,29 +52,37 @@ net_energy = produced_energy - consumed_energy
 
 # ----- Array initialisation -----
 
-time = np.linspace(0, simulation_duration, num=int(simulation_duration / tick) + 1, dtype='u4')
-speed_kmh = np.full(time.size, fill_value=speed, dtype='f4')
-delta_energy = np.full(time.size, fill_value=net_energy, dtype='f4')
-tick_array = np.full(time.size, fill_value=tick, dtype='u4')
+time = np.linspace(0, simulation_duration, num=int(simulation_duration / tick) + 1, dtype='f4')
+
+speed_kmh = np.full_like(time, fill_value=speed, dtype='f4')
+
+delta_energy = np.full_like(time, fill_value=net_energy, dtype='f4')
+
+tick_array = np.full_like(time, fill_value=tick, dtype='f4')
+tick_array[0] = 0
 
 # ----- Array calculations -----
 
 cumulative_delta_energy = np.cumsum(delta_energy)
-stored_energy, discharge_capacity, state_of_charge = basic_battery.update_array(cumulative_delta_energy)
+battery_variables = basic_battery.update_array(cumulative_delta_energy)
 
-state_of_charge = state_of_charge.round(3)
+state_of_charge = battery_variables[0].round(3)
 
 speed_kmh = np.logical_and(speed_kmh, state_of_charge) * speed_kmh
 
 time_in_motion = np.logical_and(tick_array, state_of_charge) * tick
+
 time_taken = np.sum(time_in_motion)
+time_taken = str(datetime.timedelta(seconds=int(time_taken)))
+
+final_soc = state_of_charge[-1] * 100 + 0.
 
 # ----- Target value -----
 
 distance = speed_kmh * (tick / 3600)
 distance_travelled = np.sum(distance)
 
-print(f"Time taken: {datetime.timedelta(seconds=int(time_taken))}\n"
+print(f"Time taken: {time_taken}\n"
       f"Speed: {speed}km/h\n"
       f"Maximum distance traversable: {distance_travelled:.2f}km\n"
-      f"Final battery SOC: {state_of_charge[-1] * 100 + 0.:.2f}%")
+      f"Final battery SOC: {final_soc:.2f}%\n")