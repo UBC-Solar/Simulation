c262c41929cc806c194f8712d81e6be6bd96923a
DavidWidjaja-137 <david.widjaja@alumni.ubc.ca>
2020-06-26 17:17:38 -0700

    Added scripts to calculate environmental variables

---
+++ simulation/environment/GIS.py
@@ -0,0 +1,298 @@
+#!usr/bin/env python
+
+import requests
+import json
+import polyline
+import numpy as np
+
+google_api_key = "AIzaSyCPgIT_5wtExgrIWN_Skl31yIg06XGtEHg"
+
+
+class GIS:
+
+
+    def __init__(self, api_key, origin_coord, dest_coord, waypoints):
+
+        self.R = 6371009
+
+        self.api_key = api_key
+       
+        self.current_index = 0
+        self.distance_remainder = 0 
+      
+        self.path = self.update_path(origin_coord, dest_coord, waypoints)
+
+        self.path_elevations = self.calculate_path_elevations(self.path)
+
+        self.path_distances = self.calculate_path_distances(self.path)
+         
+        self.path_gradients = self.calculate_path_gradients(self.path_elevations,\
+                                 self.path_distances)
+        
+
+    def get_path(self):
+
+        return self.path
+
+    
+    def get_path_elevations(self):
+
+        return self.path_elevations
+
+
+    def get_path_distances(self):
+
+        return self.path_distances
+
+
+    def get_path_gradients(self):
+
+        return self.path_gradients
+
+
+    def calculate_path_elevations(self, coords):
+        """
+        Returns the elevations of every coordinate the array of coordinates passed in 
+            as a coordinate
+
+        coords: A numpy array [n][latitude, longitude]
+
+        Returns: A numpy array [n][elevation] in metres
+        """
+        
+        #construct URL
+        url_head = 'https://maps.googleapis.com/maps/api/elevation/json?locations='
+   
+        location_strings = []
+        locations = ""
+        for coord in coords:
+            
+            locations = locations + "{},{}|".format(coord[0],coord[1])
+
+            if len(locations) > 8000:
+                location_strings.append(locations[:-1])
+                locations = ""
+
+        if len(locations) != 0:
+            location_strings.append(locations[:-1])
+
+        url_tail = "&key={}".format(self.api_key)
+
+        #Get elevations
+        elevations = np.zeros(len(coords))
+
+        i = 0
+        for location_string in location_strings:
+
+            url = url_head + location_string + url_tail
+
+            r = requests.get(url)
+            response = json.loads(r.text)
+
+            if response['status'] == "OK":
+                
+                for result in response['results']:
+                
+                    elevations[i] = result['elevation'] 
+                    i = i + 1
+
+            else:
+                print("Error: No elevation was found")
+
+        
+        return elevations
+
+    
+    def calculate_current_elevation(self):
+        """
+        Get the elevation of the closest point to the current point  
+        """
+    
+        return elevations[self.current_index]
+                    
+        
+    def calculate_path_distances(self, coords):
+        """
+        The coordinates are spaced quite tightly together, and they capture the 
+            features of the road. So, the lines between every pair of adjacent 
+            coordinates can be treated like a straight line, and the distances can
+            thus be obtained.
+        
+        coords: A numpy array [n][latitute, longitude]
+        
+        Returns:
+            - A numpy array [n-1][distances],
+        """
+        
+        offset = np.roll(coords, (1, 1))
+      
+        #get the latitude and longitude differences, in radians
+        diff = (coords - offset)[1:] * np.pi/180
+        diff_lat, diff_lng = np.split(diff, 2, axis=1)
+        diff_lat = np.squeeze(diff_lat)
+        diff_lng = np.squeeze(diff_lng)
+
+        print("diff_lat: {}".format(diff_lat.shape))
+        print("diff_lng: {}".format(diff_lng.shape))
+
+        #get the mean latitude for every latitude, in radians
+        mean_lat = ((coords + offset)[1:, 0] * np.pi/180)/ 2
+        cosine_mean_lat = np.cos(mean_lat)
+
+        print("cosine_mean_lat: {}".format(cosine_mean_lat.shape))
+
+        #multiply the latitude difference with the cosine_mean_latitude
+        diff_lng_adjusted = cosine_mean_lat * diff_lng 
+
+        print("diff_lng_adjusted: {}".format(diff_lng_adjusted.shape))
+
+        #square, sum and square-root
+        square_lat = np.square(diff_lat)
+        square_lng = np.square(diff_lng_adjusted)
+        square_sum = square_lat + square_lng
+        
+        path_distances = self.R * np.sqrt(square_sum)
+        
+        return path_distances  
+        
+         
+    def calculate_path_gradients(self, elevations, distances):
+        """
+        Get the approximate gradients of every point on the path.
+        
+        elevations: [n][elevations]
+        distances: [n-1][distances]
+        
+        Returns:
+            - gradients[n-1]
+        """
+        
+        offset = np.roll(elevations, 1)
+        delta_elevations = (elevations - offset)[1:]
+        
+        gradients = delta_elevations / distances
+        
+        return gradients
+        
+         
+    def calculate_current_gradient(self):
+        """
+        Get the gradient of the point closest to the current location
+        """
+        
+        return gradients[self.current_index]
+
+    
+    def calculate_current_heading(self):
+        """
+        From the current and previous coordinate, calculate the current bearing of the vehicle.
+        """
+
+        if self.current_index - 1 < 0:
+            coord_1 = self.path[self.current_index + 1]
+            coord_2 = self.path[self.current_index]
+        else:
+            coord_1 = self.path[self.current_index]
+            coord_2 = self.path[self.current_index - 1]
+
+        y = math.sin(math.radians(coord_2[1] - coord_1[1])) * \
+                math.cos(math.radians(coord_2[0]))
+
+        x = math.cos(math.radians(coord_1[0])) * \
+                math.sin(math.radians(coord_2[0])) - \
+            math.sin(math.radians(coord_1[0])) * \
+                math.sin(math.radians(coord_2[0])) * \
+                   math.cos(math.radians(coord_2[1] - coord_1[0])) 
+
+        theta = math.atan2(y, x)
+
+        bearing = ((theta * 180)/(math.pi) + 360) % 360
+
+        return bearing 
+         
+         
+    def update_vehicle_position(self, incremental_distance):
+        """
+        Returns the closest coordinate to the current coordinate 
+        """
+
+        additional_distance = self.distance_remainder + incremental_distance
+
+        while additional_distance > 0:
+
+            self.distance_remainder = additional_distance
+            additional_distance = additional_distance - \
+                                    self.path_distances[current_index]
+
+            self.current_index = self.current_index + 1
+
+        return self.current_index - 1
+
+
+    def update_path(self, origin_coord, dest_coord, waypoints):
+        """
+        Returns a path between the origin coordinate and the destination coordinate,
+            passing through a group of optional waypoints.
+
+        origin_coord: A numpy array [latitude, longitude] of the starting coordinate
+        dest_coord: A numpy array [latitude, longitude] of the destination coordinate
+        waypoint: A numpy array [n][latitude, longitude], where n<=10
+
+        Returns: A numpy array [n][latitude, longitude], marking out the path.
+
+        https://developers.google.com/maps/documentation/directions/start
+        """
+
+        #set up URL
+        url_head = "https://maps.googleapis.com/maps/api/directions/json?origin={},{}&destination={},{}".format(origin_coord[0], origin_coord[1], dest_coord[0], dest_coord[1])
+    
+        url_waypoints = ""
+        if len(waypoints) != 0:
+            
+            url_waypoints="&waypoints="
+
+            if len(waypoints) > 10:
+                print("Too many waypoints; Truncating to 10 waypoints total")
+                waypoints = waypoints[0:10]
+            
+            for waypoint in waypoints:
+                
+                url_waypoints = url_waypoints + "via:{},{}|".format(waypoint[0],\
+                                waypoint[1])
+
+            url_waypoints = url_waypoints[:-1]
+
+        url_end = "&key={}".format(self.api_key)
+
+        url = url_head + url_waypoints + url_end
+
+        print("url: {}".format(url))
+
+        #HTTP GET
+        r = requests.get(url)
+        response = json.loads(r.text)
+
+        path_points = []
+
+        #If a route is found,
+        if response['status'] == "OK":
+            print("A route was found.")
+
+            #Pick the first route in the list of available routes
+            #A route consists of a series of legs
+            for leg in response['routes'][0]['legs']:
+
+                #Every leg contains an array of steps.
+                for step in leg['steps']:
+                
+                    #every step contains an encoded polyline
+                    polyline_raw = step['polyline']['points']
+                    polyline_coords = polyline.decode(polyline_raw)
+                    path_points = path_points + polyline_coords
+
+        else:
+            print("No route was found: {}".format(response['status']))
+
+        return np.array(path_points)
+
+
---
+++ simulation/environment/WeatherForecasts.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+"""
+A class to extract local and path weather predictions such as wind_speed, 
+    wind_direction, cloud_cover and weather type
+"""
+
+import requests
+import json
+import polyline
+import time
+import numpy as np
+
+
+class WeatherForecast():
+
+
+    def __init__(self, api_key, coords, time):
+        """
+        Initializes the instance of a WeatherForecast class 
+
+        api_key: A personal openweatherapi key to access weather forecasts
+        coords: A numpy array of [latitude, longitude]
+        time: The UNIX time of initialization
+        """
+
+        self.key = api_key
+
+        self.coords = coords
+        self.last_updated_time = time
+
+        self.weather_forecast = self.update_path_weather_forecast(self, \
+                                    coords, last_updated_time)
+
+
+    def update_local_weather_forecast(self, coord):
+        """
+        Passes in a single coordinate, returns the hourly weather forecast 
+            for the coordinate
+
+        coords: A numpy array of [latitude, longitude]
+        
+        Returns:
+        - A numpy array [24][7]
+        - [24]: hours from current time
+        - [7]: (latitude, longitude, dt, wind_speed, wind_direction,
+                    cloud_cover, description_id)
+
+        For reference to the API used:
+        - https://openweathermap.org/api/one-call-api
+        """
+
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,daily&appid={}".format(coord[0], coord[1], self.key)
+
+        r = requests.get(url)
+        response = json.loads(r.text)
+
+        weather_array = np.zeros((24, 7))
+
+        for i in range(24):
+            
+            weather_array[i][0] = coord[0]
+            weather_array[i][1] = coord[1]
+            weather_array[i][2] = response["hourly"][i]["dt"]
+            weather_array[i][3] = response["hourly"][i]["wind_speed"]
+            weather_array[i][4] = response["hourly"][i]["wind_deg"]
+            weather_array[i][5] = response["hourly"][i]["clouds"]
+            weather_array[i][6] = response["hourly"][i]["weather"][0]["id"]  
+
+        return weather_array
+
+
+    def update_local_current_weather(self, coord):
+        """
+        Passes in a single coordinate, returns the current weather for the
+            coordinate
+
+        coords: A numpy array of [latitude, longitude]
+    
+        Returns:
+        - A numpy array [7]
+        - [7]: (latitude, longitude, dt, wind_speed, wind_direction,
+                    cloud_cover, description_id)
+
+        """
+
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=minutely,daily,hourly&appid={}".\
+            format(coord[0], coord[1], self.key)
+        r = requests.get(url)
+
+        response = json.loads(r.text) 
+
+        current_weather = np.zeros(7)
+
+        current_weather[0] = coord[0]
+        current_weather[1] = coord[1]
+        current_weather[2] = response["current"]["dt"]
+        current_weather[3] = response["current"]["wind_speed"]
+        current_weather[4] = response["current"]["wind_deg"]
+        current_weather[5] = response["current"]["clouds"]
+        current_weather[6] = response["current"]["weather"][0]["id"]
+
+        return current_weather
+
+
+    def update_path_weather_forecast(self, coords, last_updated_time):
+        """
+        Passes in a list of coordinates, returns the hourly weather forecast
+            for each of the coordinates
+        
+        coords: A numpy array of [coord_index][2]
+        - [2] => [latitude, longitude]
+        
+        Returns: 
+        - A numpy array [coord_index][24][7]
+        - [coord_index]: the index of the coordinates passed into the function
+        - [24]: hours from self.last_updated_time
+        - [7]: (latitude, longitude, wind_speed, wind_direction, 
+                     cloud_cover, precipitation, description)
+
+        Modifies: self.weather_forecast
+        """
+
+        num_coords = len(coords)
+
+        weather_forecast = np.zeros(num_coords)
+
+        for i, coord in enumerate(coords):
+
+            weather_forecast[i] = self.update_local_weather_forecast(coord) 
+  
+        self.coords = coords 
+        self.last_updated_time = last_updated_time
+        self.weather_forecast = weather_forecast
+
+        return weather_forecast 
+
+ 
+    def get_path_weather_forecast(self):
+        """
+        Passes in a list of coordinates, returns the hourly weather forecast
+            for each of the coordinates without calculating
+        
+        coords: A numpy array of [coord_index][2]
+        - [2] => [latitude, longitude]
+        
+        Returns: 
+        - A numpy array [coord_index][24][7]
+        - [coord_index]: the index of the coordinates passed into the function
+        - [24]: hours from self.last_updated_time
+        - [7]: (latitude, longitude, wind_speed, wind_direction, 
+                     cloud_cover, precipitation, description)
+        """
+
+        return self.weather_forecast
+
+
+    def get_closest_weather_forecast(self, coord):
+        """
+        Passes in a single coordinate, calculates the closest coordinate to that
+        coordinate, returns the forecast for the closest location
+
+        coord: A numpy array of [latitude, longitude]
+
+        Returns:
+        - A numpy array [24][7]
+        - [24]: hours from the self.last_updated_time
+        - [7]: (latitude, longitude, wind_speed, wind_direction, 
+                    cloud_cover, precipitation, description)
+        """
+
+        temp_1 = np.full((len(self.coords),2), coord)
+        temp_2 = self.coords - temp_1
+        temp_3 = np.square(temp_2)
+        temp_4 = np.sum(temp_3, axis=1)
+        temp_5 = np.sqrt(temp_4)
+
+        return weather_forecast[temp5.index(max(temp_5))]
+
+
+    def get_last_updated_time(self):
+        """
+        Returns the UNIX time in which the list of coordinates comprising the 
+            current route was updated
+        """    
+
+        return self.last_updated_time
+
+
+    def get_directional_wind_speed(self, vehicle_azimuth, wind_speed, wind_direction):
+        """
+        Returns the wind speed in m/s, in the direction opposite to the
+            bearing of the vehicle
+
+        vehicle_bearing: The azimuth angle that the vehicle is moving in, in degrees
+        wind_speed: The absolute speed of the wind in m/s
+        wind_direction: The azimuth angle of the wind, in degrees
+
+        Returns: The wind speed in the direction opposite to the bearing of the vehicle
+        """
+
+        #if driving at heading x clockwise from north
+        #wind direction at heading y clockwise from north
+
+        return wind_speed * (math.cos(math.radians(wind_direction - vehicle_azimuth)))
+        
+
+    def get_weather_advisory(self, ID):
+        """
+        Returns a string indicating the type of weather to expect, from the standardized
+            weather code passed as a parameter
+
+        https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+        """
+
+        if ID >= 200 and ID < 300:
+            return "Thunderstorm"
+        elif ID >= 300 and ID < 500:
+            return "Drizzle" 
+        elif ID >= 500 and ID < 600:
+            return "Rain"
+        elif ID >= 600 and ID < 700:
+            return "Snow"
+        elif ID == 800:
+            return "Clear"
+        else:
+            return "Unknown"
+
+
