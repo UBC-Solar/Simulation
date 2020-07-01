f949a4c14fe3ef77012fef69e7dda0fcff3a720d
mihirnimgade <mihir.nim@outlook.com>
2020-06-30 22:56:38 -0700

    Add timeit helper function

---
+++ simulation/common/helpers.py
@@ -0,0 +1,20 @@
+import functools
+import time as timer
+
+"""
+Contains the simulations helper functions.
+"""
+
+
+def timeit(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        print(f">>> Running {func.__name__!r}... \n")
+        start = timer.perf_counter()
+        value = func(*args, **kwargs)
+        stop = timer.perf_counter()
+        run_time = stop - start
+        print(f"Finished {func.__name__!r} in {run_time:.3f}s. \n")
+        return value
+
+    return wrapper_timer
