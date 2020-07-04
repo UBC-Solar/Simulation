1c5a571c89159df281561c287dede9a816baa28a
mihirnimgade <mihir.nim@outlook.com>
2020-07-04 03:43:28 -0700

    Implement get_gradients() method and add sample code

--- simulation/environment/GIS.py
+++ simulation/environment/GIS.py
@@ -60,16 +60,14 @@ class GIS:
 
     def get_gradients(self, gis_indices):
         """
-        Takes in an array of path indices, returns the road gradient at each indice
+        Takes in an array of path indices, returns the road gradient at each index
 
         :param gis_indices: (float[N]) array of path indices
 
         returns: (float[N]) array of road gradients
         """
 
-        #TODO: implement this
-
-        pass
+        return self.path_gradients[gis_indices]
 
     # ----- Getters -----
 
@@ -375,3 +373,16 @@ class GIS:
 
         return self.current_index
 
+
+if __name__ == "__main__":
+    google_api_key = "AIzaSyCPgIT_5wtExgrIWN_Skl31yIg06XGtEHg"
+
+    origin_coord = np.array([39.0918, -94.4172])
+
+    waypoints = np.array([[39.0379, -95.6764], [40.8838, -98.3734],
+                          [41.8392, -103.7115], [42.8663, -106.3372], [42.8408, -108.7452],
+                          [42.3224, -111.2973], [42.5840, -114.4703]])
+
+    dest_coord = np.array([43.6142, -116.2080])
+
+    locationSystem = GIS(api_key=google_api_key, origin_coord=origin_coord, dest_coord=dest_coord, waypoints=waypoints)
