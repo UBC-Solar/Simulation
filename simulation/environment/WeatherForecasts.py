6e8f082aae3a61323ad4d023f1c2d297110576a1
mihirnimgade <mihir.nim@outlook.com>
2020-07-26 20:30:09 +0400

    Clean up syntax and PEP8 style issues

--- simulation/environment/WeatherForecasts.py
+++ simulation/environment/WeatherForecasts.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 A class to extract local and path weather predictions such as wind_speed, 
     wind_direction, cloud_cover and weather type
@@ -7,21 +5,19 @@ A class to extract local and path weather predictions such as wind_speed,
 
 import requests
 import json
-import polyline
-import time
 import numpy as np
 import math
 import os
 from data.weather.__init__ import weather_directory
 
-class WeatherForecasts():
 
+class WeatherForecasts:
 
     def __init__(self, api_key, coords, time):
         """
         Initializes the instance of a WeatherForecast class 
 
-        api_key: A personal openweatherapi key to access weather forecasts
+        api_key: A personal OpenWeatherAPI key to access weather forecasts
         coords: A numpy array of [latitude, longitude]
         time: The UNIX time of initialization
         """
@@ -42,28 +38,31 @@ class WeatherForecasts():
         if os.path.isfile(weather_file):
             with np.load(weather_file) as weather_data:
 
-                if (weather_data['origin_coord'] == origin_coord) and (weather_data['dest_coord'] == dest_coord):
-                    print("Previous save file is being used...")
+                if (weather_data['origin_coord'] == origin_coord).all() and \
+                   (weather_data['dest_coord'] == dest_coord).all():
+                    print("Previous weather save file is being used...\n")
                     self.weather_forecast = weather_data['weather_forecast']
 
                 else:
-                    print("Save file doesn't not exist. Calling API and creating save file...")
+                    print("Alternate weather data requested. "
+                          "Calling OpenWeather API and creating weather save file...\n")
                     self.weather_forecast = self.update_path_weather_forecast(coords, self.last_updated_time)
 
                     with open(weather_file, 'wb') as f:
                         np.savez(f, weather_forecast=self.weather_forecast, origin_coord=origin_coord,
-                            dest_coord=dest_coord)
+                                 dest_coord=dest_coord)
 
         # otherwise call API and then save arrays to file
         else:
-            print("Save file doesn't not exist. Calling API and creating save file...")
+            print("Weather save file does not exist. Calling OpenWeather API and creating save file...\n")
             self.weather_forecast = self.update_path_weather_forecast(coords, self.last_updated_time)
 
             with open(weather_file, 'wb') as f:
                 np.savez(f, weather_forecast=self.weather_forecast, origin_coord=origin_coord,
                          dest_coord=dest_coord)
 
-    def cull_dataset(self, coords):
+    @staticmethod
+    def cull_dataset(coords):
         """
         As we currently have a limited number of API calls(60) every minute with the 
             current Weather API, we must shrink the dataset significantly. As the
@@ -94,7 +93,8 @@ class WeatherForecasts():
         - https://openweathermap.org/api/one-call-api
         """
 
-        url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,daily&appid={}".format(coord[0], coord[1], self.key)
+        url = f"https://api.openweathermap.org/data/2.5/onecall?lat={coord[0]}&lon={coord[1]}" \
+              f"&exclude=current,minutely,daily&appid={self.key}"
 
         r = requests.get(url)
         response = json.loads(r.text)
@@ -102,18 +102,17 @@ class WeatherForecasts():
         weather_array = np.zeros((24, 7))
 
         for i in range(24):
-            
             weather_array[i][0] = coord[0]
             weather_array[i][1] = coord[1]
             weather_array[i][2] = response["hourly"][i]["dt"]
             weather_array[i][3] = response["hourly"][i]["wind_speed"]
             weather_array[i][4] = response["hourly"][i]["wind_deg"]
             weather_array[i][5] = response["hourly"][i]["clouds"]
-            weather_array[i][6] = response["hourly"][i]["weather"][0]["id"]  
+            weather_array[i][6] = response["hourly"][i]["weather"][0]["id"]
 
         return weather_array
 
-    def update_local_current_weather(self, coord, time):
+    def update_local_current_weather(self, coord, time=0):
         """
         Passes in a single coordinate, returns the current weather for the
             coordinate
@@ -127,11 +126,11 @@ class WeatherForecasts():
 
         """
 
-        url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=minutely,daily,hourly&appid={}".\
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=minutely,daily,hourly&appid={}". \
             format(coord[0], coord[1], self.key)
         r = requests.get(url)
 
-        response = json.loads(r.text) 
+        response = json.loads(r.text)
 
         current_weather = np.zeros(7)
 
@@ -162,26 +161,25 @@ class WeatherForecasts():
 
         Modifies: self.weather_forecast
         """
-        
+
         num_coords = len(coords)
-        
+
         weather_forecast = np.zeros((num_coords, 24, 7))
-        
+
         for i, coord in enumerate(coords):
-       
-            #If the limit has not been reached, then continue getting the hourly forecast 
+
+            # If the limit has not been reached, then continue getting the hourly forecast
             if i < 10:
                 weather_forecast[i] = self.update_local_weather_forecast(coord)
             else:
-                weather_forecast[i] = np.zeros((24,7))
-                
-  
-        self.coords = coords 
+                weather_forecast[i] = np.zeros((24, 7))
+
+        self.coords = coords
         self.last_updated_time = last_updated_time
         self.weather_forecast = weather_forecast
 
-        return self.weather_forecast 
- 
+        return self.weather_forecast
+
     def get_path_weather_forecast(self):
         """
         Passes in a list of coordinates, returns the hourly weather forecast
@@ -200,7 +198,8 @@ class WeatherForecasts():
 
         return self.weather_forecast
 
-    def calculate_closest_weather_indices(self, cumulative_distances, cumulative_distances_gis):
+    @staticmethod
+    def calculate_closest_weather_indices(cumulative_distances, cumulative_distances_gis):
         """
         Takes in an array of point distances from starting point, returns a list of 
             weather_forecast indices in the region closest to the point
@@ -213,29 +212,25 @@ class WeatherForecasts():
         """
 
         indices = np.zeros(len(cumulative_distances))
-        i = 0
-        j = 0
+        i, j = 0, 0
         while i < len(cumulative_distances) and j < len(cumulative_distances_gis):
-    
+
             if cumulative_distances[i] > cumulative_distances_gis[j]:
-                
-                #count upwards on cumulative_distances, append current value of j
-                indices[i] = j/625
-                i = i + 1
+                # count upwards on cumulative_distances, append current value of j
+                indices[i] = j / 625
+                i += 1
 
             elif cumulative_distances[i] < cumulative_distances_gis[j]:
-
-                #count upwards on cumulative_distances_gis
-                j = j + 1
+                # count upwards on cumulative_distances_gis
+                j += 1
 
             else:
-            
-                #count upwards on both, append current value of j
-                indices[i] = j/625
-                i = i + 1
-                j = j + 1
+                # count upwards on both, append current value of j
+                indices[i] = j / 625
+                i += 1
+                j += 1
 
-        return indices 
+        return indices
 
     def get_weather_forecasts(self, indices):
         """
@@ -267,7 +262,7 @@ class WeatherForecasts():
                     cloud_cover, precipitation, description)
         """
 
-        temp_1 = np.full((len(self.coords),2), coord)
+        temp_1 = np.full((len(self.coords), 2), coord)
         temp_2 = self.coords - temp_1
         temp_3 = np.square(temp_2)
         temp_4 = np.sum(temp_3, axis=1)
@@ -279,11 +274,12 @@ class WeatherForecasts():
         """
         Returns the UNIX time in which the list of coordinates comprising the 
             current route was updated
-        """    
+        """
 
         return self.last_updated_time
 
-    def get_directional_wind_speed(self, vehicle_azimuth, wind_speed, wind_direction):
+    @staticmethod
+    def get_directional_wind_speed(vehicle_azimuth, wind_speed, wind_direction):
         """
         Returns the wind speed in m/s, in the direction opposite to the
             bearing of the vehicle
@@ -297,7 +293,8 @@ class WeatherForecasts():
 
         return wind_speed * (math.cos(math.radians(wind_direction - vehicle_azimuth)))
 
-    def get_array_directional_wind_speed(self, vehicle_bearings, wind_speeds, wind_directions):
+    @staticmethod
+    def get_array_directional_wind_speed(vehicle_bearings, wind_speeds, wind_directions):
         """
         Returns the array of wind speed in m/s, in the direction opposite to the 
             bearing of the vehicle
@@ -308,10 +305,11 @@ class WeatherForecasts():
 
         Returns: The wind speeds in the direction opposite to the bearing of the vehicle
         """
-        
-        return wind_speeds * (np.cos(np.radians(wind_directions - vehicle_bearings))) 
-        
-    def get_weather_advisory(self, ID):
+
+        return wind_speeds * (np.cos(np.radians(wind_directions - vehicle_bearings)))
+
+    @staticmethod
+    def get_weather_advisory(ID):
         """
         Returns a string indicating the type of weather to expect, from the standardized
             weather code passed as a parameter
@@ -319,16 +317,15 @@ class WeatherForecasts():
         https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
         """
 
-        if ID >= 200 and ID < 300:
+        if 200 <= ID < 300:
             return "Thunderstorm"
-        elif ID >= 300 and ID < 500:
-            return "Drizzle" 
-        elif ID >= 500 and ID < 600:
+        elif 300 <= ID < 500:
+            return "Drizzle"
+        elif 500 <= ID < 600:
             return "Rain"
-        elif ID >= 600 and ID < 700:
+        elif 600 <= ID < 700:
             return "Snow"
         elif ID == 800:
             return "Clear"
         else:
             return "Unknown"
-
