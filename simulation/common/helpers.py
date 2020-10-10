01ffd0ac8e44ed6cd2adab8016765d230e105df1
mihirnimgade <mihir.nim@outlook.com>
2020-10-11 00:29:13 +0530

    Fix syntax error

--- simulation/common/helpers.py
+++ simulation/common/helpers.py
@@ -12,7 +12,8 @@ def timeit(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
         print(f">>> Running {func.__name__!r}... \n")
-        start = timer.perf_counter()        value = func(*args, **kwargs)
+        start = timer.perf_counter()
+        value = func(*args, **kwargs)
         stop = timer.perf_counter()
         run_time = stop - start
         print(f"Finished {func.__name__!r} in {run_time:.3f}s. \n")
