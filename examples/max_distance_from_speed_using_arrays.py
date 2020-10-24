e6bb4f98723860512018b5d3e212523829fbad6a
Chris Chang <1chang1chang1@gmail.com>
2020-10-24 14:56:30 -0700

    Added waypoints for FSGP

--- examples/max_distance_from_speed_using_arrays.py
+++ examples/max_distance_from_speed_using_arrays.py
@@ -15,6 +15,8 @@ def main():
 
     input_speed = np.array([35])
 
+    race_type = "FSGP"
+
     """
     Note: it no longer matters how many elements the input_speed array has, the simulation automatically
         reshapes the array depending on the simulation_length. 
@@ -38,16 +40,28 @@ def main():
     google_api_key = "AIzaSyCPgIT_5wtExgrIWN_Skl31yIg06XGtEHg"
     weather_api_key = "51bb626fa632bcac20ccb67a2809a73b"
 
-    origin_coord = np.array([39.0918, -94.4172])
+    if race_type == "FSGP":
+        origin_coord = np.array([38.9267957, -95.67676127])
+        # Waypoints obtained from Google Maps 
+        waypoints = np.array([38.9272094, -95.6771353], [38.9189985, -95.6752748],
+                            [38.9215404, -95.6744271], [38.920318, -95.6709074]
+                             [38.9250241,-95.6714015], [38.929093, -95.6774928],
+                             [38.929093,-95.6774928], [38.9271993, -95.6771334])
 
-    waypoints = np.array([[39.0379, -95.6764], [40.8838, -98.3734],
+        dest_coord = np.array([38.9243266,-95.6799917])
+
+    elif race_type == "ASC":
+
+        origin_coord = np.array([39.0918, -94.4172])
+
+        waypoints = np.array([[39.0379, -95.6764], [40.8838, -98.3734],
                           [41.8392, -103.7115], [42.8663, -106.3372], [42.8408, -108.7452],
                           [42.3224, -111.2973], [42.5840, -114.4703]])
 
-    dest_coord = np.array([43.6142, -116.2080])
+        dest_coord = np.array([43.6142, -116.2080])
 
     simulation_model = simulation.Simulation(google_api_key, weather_api_key, origin_coord, dest_coord, waypoints,
-                                             tick=1, simulation_duration=simulation_length, race_type="FSGP")
+                                                tick=1, simulation_duration=simulation_length, race_type="FSGP")
 
     for _ in range(1):
         distance_travelled = simulation_model.run_model(speed=input_speed, plot_results=True)
