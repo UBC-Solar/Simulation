a48c60f09d52ab4686d816602b18cec74ba27b75
mihirnimgade <mihir.nim@outlook.com>
2021-05-23 19:34:17 -0700

    Update SimulationResult class to contain a dictionary of arrays rather than a list of arrays

--- simulation/main/SimulationResult.py
+++ simulation/main/SimulationResult.py
@@ -1,11 +1,16 @@
+from collections import OrderedDict
+
+
 class SimulationResult:
-    def __init__(self, arrays=None, distance_travelled=None, time_taken=None, final_soc=None):
+    def __init__(self, array_dict: OrderedDict, time_taken: str, distance_travelled: float, average_speed: float,
+                 final_soc: float):
         """
         Instantiates a SimulationResult object. This is used in the MainSimulation class when
         running a simulation. This object simply stores desired simulation results while the
         simulation is running its calculations to better encapsulate the information
         """
-        self.arrays = arrays
-        self.distance_travelled = distance_travelled
+        self.array_dict = array_dict
         self.time_taken = time_taken
+        self.distance_travelled = distance_travelled
+        self.average_speed = average_speed
         self.final_soc = final_soc
