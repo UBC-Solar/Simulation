bac284bc72803ce783cc721c39ff2aebcb5b37cb
Chris Chang <64388914+chrischang5@users.noreply.github.com>
2022-03-24 11:38:18 -0700

    Race Checkpoints and Go Speedup  (#115)
    * Removed old Simulation example files, optimization testing, scripts folder
    
    * Add GoLang files
    
    * Update tests and add test_weather.py
    
    * Update get_weather_in_time with Go implementation option and flag
    
    * Implemented helper function speeds_with_waypoints (#111)
    
    * Added the python file where testing and of the race checkpoint task will
    be developed
    
    * Moved some of the code that Chris had from Google Colab to my local
    python file for implementation
    
    * Upload coord_by_time notebook for prototyping
    
    * Not using task_race_checkpoints.py anymore
    
    * Added the frame work for Step 4 of the implementatin of race_checkpoints
    
    * Started the implementation of step4_match_coords and wrote in comments
    on how the function will behave given the three scenarios when the speed
    value and the distance between coordinates.
    
    * Changed branch name
    
    * Added step 4
    
    * Added delta factor to step 4
    
    * Updated some changes into Visualize Helper to include waypoints (checkpoints)
    
    * Interim commit for step 5
    
    * Cleaned up step 5
    
    * Step 5 complete
    
    * Updated comments and skimmed unused code
    
    * Updated route_visualization to not accept waypoints
    
    Co-authored-by: Jason Liang <jasonliang2002@gmail.com>
    Co-authored-by: Chris Chang <64388914+chrischang5@users.noreply.github.com>
    
    * Integrate Race Checkpoints (#114)
    
    * Added the python file where testing and of the race checkpoint task will
    be developped
    
    * Moved some of the code that Chris had from Google Colab to my local
    python file for implementation
    
    * Upload coord_by_time notebook for prototyping
    
    * Not using task_race_checkpoints.py anymore
    
    * Added the frame work for Step 4 of the implementatin of race_checkpoints
    
    * Started the implementation of step4_match_coords and wrote in comments
    on how the function will behave given the three scenarios when the speed
    value and the distance between coordinates.
    
    * Changed branch name
    
    * Added step 4
    
    * Added delta factor to step 4
    
    * Updated some changes into Visualize Helper to include waypoints (checkpoints)
    
    * Interim commit for step 5
    
    * Cleaned up step 5
    
    * Step 5 complete
    
    * Updated comments and skimmed unused code
    
    * Updated route_visualization to not accept waypoints
    
    * Integrate Race Checkpoints Code
    
    Co-authored-by: Jason Liang <jasonliang2002@gmail.com>
    Co-authored-by: ritchiexia <ritchie.xia@gmail.com>
    
    Co-authored-by: ritchiexia <64629481+ritchiexia@users.noreply.github.com>
    Co-authored-by: Jason Liang <jasonliang2002@gmail.com>
    Co-authored-by: ritchiexia <ritchie.xia@gmail.com>

--- examples/max_distance_from_speed_using_arrays.py
+++ examples/max_distance_from_speed_using_arrays.py
@@ -14,9 +14,8 @@ before the battery runs out [speed -> distance].
 
 @helpers.timeit
 def main():
-    # indicates a constant speed of 35km/h throughout the simulation
 
-    input_speed = np.array([50] * 24)
+    input_speed = np.array([30])
 
     """
     Note: it no longer matters how many elements the input_speed array has, the simulation automatically
@@ -38,15 +37,14 @@ def main():
       Keep in mind, however, that the condition len(input_speed) <= simulation_length must be true
     """
 
-    simulation_model = simulation.Simulation(race_type="FSGP")
-    distance_travelled = simulation_model.run_model(speed=input_speed, plot_results=True,
-                                                    verbose=False,
-                                                    route_visualization=False)
+    simulation_model = simulation.Simulation(race_type="ASC")
+    distance_travelled = simulation_model.run_model(speed=input_speed, plot_results=True, verbose=False)
 
     bounds = InputBounds()
     bounds.add_bounds(8, 20, 60)
     optimization = BayesianOptimization(bounds, simulation_model.run_model)
-    random_optimization = RandomOptimization(bounds, simulation_model.run_model)
+    random_optimization = RandomOptimization(
+        bounds, simulation_model.run_model)
 
     results = optimization.maximize(init_points=3, n_iter=1, kappa=10)
     optimized = simulation_model.run_model(speed=np.fromiter(results, dtype=float), plot_results=True,
