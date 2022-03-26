3a5858f50ca230db8651c8782ba3427df8ac6b45
chrischang5 <1chang1chang1@gmail.com>
2022-03-26 14:54:56 -0700

    Format code

--- simulation/optimization/base_optimization.py
+++ simulation/optimization/base_optimization.py
@@ -1,14 +1,15 @@
 from abc import abstractmethod
 from simulation.utils.InputBounds import InputBounds
 
-class BaseOptimization:
-  def __init__(self, bounds: InputBounds, f):
-    self.bounds = bounds
-    self.func = f
-    self.result = None
-    self.target = float('-inf')
-    self.bestinput = []
 
-  @abstractmethod
-  def maximize(self):
-    raise NotImplementedError()
+class BaseOptimization:
+    def __init__(self, bounds: InputBounds, f):
+        self.bounds = bounds
+        self.func = f
+        self.result = None
+        self.target = float('-inf')
+        self.bestinput = []
+
+    @abstractmethod
+    def maximize(self):
+        raise NotImplementedError()
