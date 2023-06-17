0acb69b98549713072d3454cf147a09a9a7d84db
Renu <aerorenu@gmail.com>
2023-06-17 11:44:27 -0700

    started working on regen

--- simulation/regen/basic_regen.py
+++ simulation/regen/basic_regen.py
@@ -1,10 +1,16 @@
 from simulation.regen.base_regen import BaseRegen
+import numpy as np
 
 
 class BasicRegen(BaseRegen):
     def __init__(self):
         super().__init__()
 
-    def update(self, tick):
+    def update(self, tick, speed_kmh):
+        acceleration = np.diff(speed_kmh)
+
+        deceleration_instances = np.where(acceleration < 0, acceleration, np.zeros(len(acceleration)))
+        print(deceleration_instances)
+
         self.produced_energy = 0
         return self.produced_energy * tick
