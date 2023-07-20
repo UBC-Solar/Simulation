1a0247933616aafc1bd0e9cee30c6e03b209a7bb
Joshua Riefman <joshuariefman@gmail.com>
2023-07-20 12:43:11 -0700

    Move to use a class container for optimization settings

--- simulation/optimization/genetic.py
+++ simulation/optimization/genetic.py
@@ -27,62 +27,125 @@ See the following resources for explanations of genetic algorithms and different
 """
 
 
+class Parent_Selection_Type(Enum):
+    sss = 0
+    tournament = 1
+    stochastic = 2
+    rank = 3
+
+    def __str__(self):
+        if self.value == 0:
+            return "sss"
+        elif self.value == 1:
+            return "tournament"
+        elif self.value == 2:
+            return "stochastic"
+        else:
+            return "rank"
+
+
+class Mutation_Type(Enum):
+    random = 0
+
+    def __str__(self):
+        if self.value == 0:
+            return "random"
+
+
+class Crossover_Type(Enum):
+    single_point = 0
+    two_points = 1
+    scattered = 2
+    uniform = 3
+
+    def __str__(self):
+        if self.value == 0:
+            return "single_point"
+        elif self.value == 1:
+            return "two_points"
+        elif self.value == 2:
+            return "scattered"
+        else:
+            return "uniform"
+
+
+class OptimizationSettings:
+    def __init__(self, chromosome_size=4,
+                 parent_selection_type=Parent_Selection_Type.sss,
+                 generation_limit=1,
+                 num_parents=4,
+                 k_tournament=4,
+                 crossover_type=Crossover_Type.two_points,
+                 elitism=3,
+                 mutation_type=Mutation_Type.random,
+                 mutation_percent=25,
+                 max_mutation=0.05):
+        self.chromosome_size: int = chromosome_size
+        self.parent_selection_type: Parent_Selection_Type = parent_selection_type
+        self.generation_limit: int = generation_limit
+        self.num_parents: int = num_parents
+        self.k_tournament: int = k_tournament
+        self.crossover_type: Crossover_Type = crossover_type
+        self.elitism: int = elitism
+        self.mutation_type: Mutation_Type = mutation_type
+        self.mutation_percent: float = mutation_percent
+        self.max_mutation: float = max_mutation
+
+        self._fitness: float = 0
+
+    def as_list(self):
+        out_list: list = [str(self.chromosome_size), str(self.parent_selection_type), str(self.generation_limit),
+                          str(self.num_parents), str(self.k_tournament), str(self.crossover_type),str(self.elitism),
+                          str(self.mutation_type), str(self.mutation_percent), str(self.max_mutation)]
+        return out_list
+
+    def set_fitness(self, fitness):
+        self._fitness = fitness
+
+
 class GeneticOptimization(BaseOptimization):
-    class Crossover_Type(Enum):
-        single_point = 0
-        two_points = 1
-        scattered = 2
-        uniform = 3
 
-    class Parent_Selection_Type(Enum):
-        sss = 0
-        tournament = 1
-        stochastic = 2
-        rank = 3
-
-    class Mutation_Type(Enum):
-        random = 0
-
-    def __init__(self, model: Simulation, bounds: InputBounds, input_speed: np.ndarray, force_new_population_flag=True):
+    def __init__(self, model: Simulation, bounds: InputBounds, input_speed: np.ndarray, force_new_population_flag=False, settings: OptimizationSettings = OptimizationSettings()):
         super().__init__(bounds, model.run_model)
         self.model = model
         self.bounds = bounds.get_bounds_list()
         fitness_function = self.fitness
+        self.settings = settings
 
         # Define how many iterations that GA will run
-        num_generations = 1
+        num_generations = self.settings.generation_limit
 
         # Define how many parents will be used for the creation of offspring for each subsequent generation
-        num_parents_mating = 4
+        num_parents_mating = self.settings.num_parents
 
         # Define the size of the population (number of chromosomes)
         # More chromosomes means a larger gene pool (should result in superior optimization) but at the cost of
         # needing to calculate the fitness of more chromosomes.
-        self.sol_per_pop = 12
+        self.sol_per_pop = self.settings.chromosome_size
 
         # Define how parents are selected from the population
         # 'tournament' will result in tournament selection, 'sss' for steady-state selection,
-        parent_selection_type = GeneticOptimization.Parent_Selection_Type.tournament
+        parent_selection_type = self.settings.parent_selection_type
 
         # Define number of chromosomes in each tournament
-        K_tournament = 4
+        K_tournament = self.settings.k_tournament
 
         # Define the number of the best chromosomes that will be kept in the next generation
-        keep_elitism = 3
+        keep_elitism = self.settings.elitism
 
         # Define the type of crossover that will be used in offspring creation
         # 'single_point' for single-point crossover, 'two_points' for double-point, 'scattered' for scattered crossover
         # and 'uniform' for uniform crossover.
-        crossover_type = GeneticOptimization.Crossover_Type.two_points
+        crossover_type = self.settings.crossover_type
 
         # Define the type of mutation that will be used in offspring creation
-        mutation_type = GeneticOptimization.Mutation_Type.random
+        mutation_type = self.settings.mutation_type
 
         # Define the number of genes that will be mutated (0 <= x < 1)
-        mutation_percent_genes = 25
+        mutation_percent_genes = self.settings.mutation_percent
 
         # Define a maximum value for gene value mutations (should be 0 < x < 1)
-        mutation_max_value = 0.05
+        mutation_max_value = self.settings.max_mutation
 
         # Bound the value of each gene to be between 0 and 1 as chromosomes should be normalized.
         gene_space = {'low': 0.0, 'high': 1.0}
@@ -90,19 +153,23 @@ class GeneticOptimization(BaseOptimization):
         # Add a time delay between generations (used for debug purposes)
         delay_after_generation = 0.0
 
+        # We must obtain or create an initial population for GA to work with.
         initial_population = self.get_initial_population(input_speed, self.sol_per_pop, force_new_population_flag)
 
+        # This informs GA when to end the optimization sequence. If blank, it will continue until the generation
+        # iterations finish. Write "saturate_x" for the sequence to end after x generations of no improvement to
+        # fitness. Write "reach_x" for the sequence to end after fitness has reached x.
         stop_criteria = "saturate_10"
 
         self.ga_instance = pygad.GA(num_generations=num_generations,
                                     initial_population=initial_population,
                                     num_parents_mating=num_parents_mating,
                                     fitness_func=fitness_function,
-                                    parent_selection_type=parent_selection_type,
+                                    parent_selection_type=str(parent_selection_type),
                                     K_tournament=K_tournament,
                                     keep_elitism=keep_elitism,
-                                    crossover_type=crossover_type,
-                                    mutation_type=mutation_type,
+                                    crossover_type=str(crossover_type),
+                                    mutation_type=str(mutation_type),
                                     mutation_percent_genes=mutation_percent_genes,
                                     gene_space=gene_space,
                                     on_generation=lambda x: print("New generation!"),
@@ -134,7 +201,7 @@ class GeneticOptimization(BaseOptimization):
         lower_stretch_bound = 0.5
 
         if not self.model.check_if_has_calculated(raiseException=False):
-            self.model.run_model(speed=input_speed, plot_results=True)
+            self.model.run_model(speed=input_speed, plot_results=False)
         SOC = self.model.get_results(["raw_soc"])
         speed_arrays = []
 
@@ -172,6 +239,7 @@ class GeneticOptimization(BaseOptimization):
         print("Parameters of the best solution : {solution}".format(solution=self.bestinput))
         print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
         self.plot_fitness()
+        self.settings.set_fitness(solution_fitness)
         return self.bestinput
 
     def plot_fitness(self, reset_register=False):
