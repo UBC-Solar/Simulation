851e1d9538b5ad5b4f9b64d396b5fe990ba01829
Joshua Riefman <joshuariefman@gmail.com>
2023-09-16 14:34:40 -0700

    Improve plotting with plot_portion parameter

--- simulation/utils/Plotting.py
+++ simulation/utils/Plotting.py
@@ -26,20 +26,25 @@ class Plotting:
     def __init__(self):
         self.graph_queue = []
 
-    def plot_graphs(self, timestamps):
+    def plot_graphs(self, timestamps, plot_portion):
         """
 
         Plot all graphs in the graph queue.
 
         :param np.ndarray timestamps: Array of timestamps which serves as the "x-axis" of graphs.
+        :param tuple[float] plot_portion: A tuple containing percentages that denote the beginning and end of the
+        portion of the race that we'd like to plot. For example, if you wanted to plot the second half of the race,
+        input (0.5, 1.0).
 
         """
+
         # Graph all graphs that have been queued
         for graph in self.graph_queue:
             plot_graph(timestamps=timestamps,
                        arrays_to_plot=graph.arrays_to_plot,
                        array_labels=graph.plot_titles,
-                       graph_title=graph.graph_name)
+                       graph_title=graph.graph_name,
+                       plot_portion=plot_portion)
 
         # Remove every graph from the queue after they've been graphed.
         self.graph_queue.clear()
