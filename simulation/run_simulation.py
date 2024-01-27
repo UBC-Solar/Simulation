59313577c207fcf900879d62989349070128bc3e
Joshua Riefman <joshuariefman@gmail.com>
2024-01-27 09:13:06 -0800

    Do some hyperparameter testing

--- simulation/run_simulation.py
+++ simulation/run_simulation.py
@@ -106,6 +106,8 @@ def run_simulation(settings):
     bounds = InputBounds()
     bounds.add_bounds(driving_hours, minimum_speed, maximum_speed)
 
+    run_hyperparameter_search(simulation_model, bounds)
+
     # Perform optimization with Genetic Optimization
     optimization_settings: OptimizationSettings = OptimizationSettings()
     with tqdm(total=optimization_settings.generation_limit, file=sys.stdout, desc="Optimizing driving speeds",
@@ -113,27 +115,6 @@ def run_simulation(settings):
         geneticOptimization = GeneticOptimization(simulation_model, bounds, settings=optimization_settings, pbar=pbar)
         results_genetic = geneticOptimization.maximize()
     optimized_genetic = simulation_model.run_model(geneticOptimization.bestinput, plot_results=True)
-
-    # Initialize optimization methods
-    optimization = BayesianOptimization(bounds, simulation_model.run_model)
-    random_optimization = RandomOptimization(bounds, simulation_model.run_model)
-
-    # Perform optimization with Bayesian Optimization
-    results_bayesian = optimization.maximize(init_points=5, n_iter=settings.optimization_iterations, kappa=10)
-    optimized_bayesian = simulation_model.run_model(speed=np.fromiter(results_bayesian, dtype=float), plot_results=True,
-                                                    verbose=settings.verbose,
-                                                    route_visualization=settings.route_visualization)
-
-    # Perform optimization with random optimization
-    results_random = random_optimization.maximize(iterations=settings.optimization_iterations)
-    optimized_random = simulation_model.run_model(speed=np.fromiter(results_random, dtype=float), plot_results=True,
-                                                  verbose=settings.verbose,
-                                                  route_visualization=settings.route_visualization)
-
-    #  ----- Output results ----- #
-
-    display_output(settings.return_type, unoptimized_time, optimized_bayesian, optimized_random, results_bayesian,
-                   results_random)
 
     return unoptimized_time
 
