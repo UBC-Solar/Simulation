5717091aefb009fecb762cace4bd7bb74bd02151
Joshua Riefman <joshuariefman@gmail.com>
2024-04-08 19:39:19 -0700

    Generalize decryption script

--- simulation/cmd/decrypt_data.py
+++ simulation/cmd/decrypt_data.py
@@ -5,6 +5,7 @@ Requires the encryption key to be in the user's .env.
 
 import os
 from simulation.cache.weather import weather_directory
+from pathlib import Path
 from simulation.utils import Decryptor, Key
 from dotenv import load_dotenv
 
@@ -12,14 +13,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def decrypt(decryptor: Decryptor, src: Path, dest: Path) -> None:
+    """
+    Decrypt the data in ``src`` using ``decryptor`` and write it to ``dest``.
+    :param decryptor: Decryptor to perform the decryption
+    :param src: Source file to be decrypted
+    :param dest: Destination file for decrypted data, will be overwritten
+    """
+
+    with open(src, 'rb') as file:
+        encrypted_data: bytes = file.read()
+
+    decrypted_data: bytes = decryptor.decrypt(encrypted_data)
+
+    with open(dest, 'wb') as file:
+        file.write(decrypted_data)
+
+
 if __name__ == "__main__":
     key_str = os.getenv("ENCRYPTION_KEY")
     decryptor = Decryptor(Key.from_str(key_str))
 
-    with open(weather_directory / 'weather_data_FSGP_SOLCAST.bin', 'rb') as file:
-        encrypted_data: bytes = file.read()
-
-    decrypted_data: bytes = decryptor.decrypt(encrypted_data)
-    print(decrypted_data)
-    with open(weather_directory / 'weather_data_FSGP_SOLCAST.npz', 'wb') as file:
-        file.write(decrypted_data)
+    decrypt(decryptor, weather_directory / 'weather_data_FSGP_SOLCAST.bin', weather_directory / 'weather_data_FSGP_SOLCAST.npz' )
