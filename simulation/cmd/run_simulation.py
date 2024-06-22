e9aa40c5b9efbb3f30b5b2865ff279c9871d4471
Joshua Riefman <joshuariefman@gmail.com>
2024-06-22 16:03:36 -0700

    Revert "Improve argument parsing, seperate scripts into hyperparameter search, run simulation, and optimize simulation"
    This reverts commit 4039cd7f7554d0b1c9611673e9750cab37bfc443.

--- simulation/cmd/run_simulation.py
+++ simulation/cmd/run_simulation.py
@@ -1,17 +1,23 @@
-import argparse
-
 import numpy as np
 import subprocess
+import datetime
 import json
 import sys
+import csv
 
 from simulation.model.Simulation import Simulation, SimulationReturnType
 from simulation.utils.InputBounds import InputBounds
-from simulation.config import config_directory, speeds_directory
+from simulation.config import config_directory
 from simulation.utils.SimulationBuilder import SimulationBuilder
 from simulation.optimization.genetic import GeneticOptimization, OptimizationSettings
+from simulation.data.results import results_directory
+from simulation.data.assemble import Assembler
 from simulation.common.race import Race
 from tqdm import tqdm
+
+"""
+Description: Execute simulation optimization sequence. 
+"""
 
 
 class SimulationSettings:
@@ -21,23 +27,54 @@ class SimulationSettings:
 
     """
 
-    def __init__(self, race_type="FSGP", verbose=False, granularity=1):
+    def __init__(self, race_type="FSGP", golang=True, return_type=SimulationReturnType.distance_and_time,
+                 optimization_iterations=20, route_visualization=False, verbose=False, granularity=1):
+
         self.race_type = race_type
-        self.return_type = SimulationReturnType.distance_and_time
-        self.route_visualization = False
+        self.optimization_iterations = optimization_iterations
+        self.golang = golang
+        self.return_type = return_type
+        self.route_visualization = route_visualization
         self.verbose = verbose
         self.granularity = granularity
 
+    def __str__(self):
+        return (f"GoLang is {str('enabled.' if self.golang else 'disabled.')}\n"
+                f"Verbose is {str('on.' if self.verbose else 'off.')}\n"
+                f"Route visualization is {str('on.' if self.route_visualization else 'off.')}\n"
+                f"Optimizing for {str('time.' if self.return_type == 0 else 'distance.')}\n"
+                f"Will perform {self.optimization_iterations} optimization iterations.\n")
 
-def main(settings: SimulationSettings, speeds_filename: str):
+
+def main():
     """
+
     This is the entry point to Simulation.
+    First, parse command line arguments, then execute simulation optimization sequence.
+
+    """
+
+    #  ----- Parse commands passed from command line ----- #
+
+    cmds = sys.argv
+    simulation_settings = parse_commands(cmds)
+
+    print(str(simulation_settings))
+
+    #  ----- Run simulation ----- #
+
+    run_simulation(simulation_settings)
+
+    print("Simulation has completed.")
+
+
+def run_simulation(settings):
+    """
 
     This method parses initial conditions for the simulation and store them in a simulationState object. Then, begin
     optimizing simulation with Bayesian optimization and then random optimization.
 
     :param SimulationSettings settings: object that stores settings for the simulation and optimization sequence
-    :param str speeds_filename: name of the cached speeds file to use, otherwise a default array is used
     :return: returns the time taken for simulation to complete before optimization
     :rtype: float
 
@@ -48,6 +85,7 @@ def main(settings: SimulationSettings, speeds_filename: str):
     simulation_builder = SimulationBuilder() \
         .set_initial_conditions(initial_conditions) \
         .set_model_parameters(model_parameters, Race.RaceType(settings.race_type)) \
+        .set_golang(settings.golang) \
         .set_return_type(settings.return_type) \
         .set_granularity(settings.granularity)
 
@@ -55,24 +93,166 @@ def main(settings: SimulationSettings, speeds_filename: str):
 
     # Initialize a "guess" speed array
     driving_hours = simulation_model.get_driving_time_divisions()
-
-    if speeds_filename is None:
-        input_speed = np.array([60] * driving_hours)
-    else:
-        input_speed = np.load(speeds_directory / (speeds_filename + ".npy"))
-        if len(input_speed) != driving_hours:
-            raise ValueError(f"Cached speeds {speeds_filename} has improper length!")
+    input_speed = np.array([60] * driving_hours)
 
     # Run simulation model with the "guess" speed array
     unoptimized_time = simulation_model.run_model(speed=input_speed, plot_results=True,
                                                   verbose=settings.verbose,
                                                   route_visualization=settings.route_visualization)
 
+    # Set up optimization models
+    maximum_speed = 60
+    minimum_speed = 0
+
+    bounds = InputBounds()
+    bounds.add_bounds(driving_hours, minimum_speed, maximum_speed)
+
+    run_hyperparameter_search(simulation_model, bounds)
+
+    # Perform optimization with Genetic Optimization
+    optimization_settings: OptimizationSettings = OptimizationSettings()
+    with tqdm(total=optimization_settings.generation_limit, file=sys.stdout, desc="Optimizing driving speeds",
+              position=0, leave=True, unit="Generation", smoothing=1.0) as pbar:
+        geneticOptimization = GeneticOptimization(simulation_model, bounds, settings=optimization_settings, pbar=pbar)
+        results_genetic = geneticOptimization.maximize()
+    optimized_genetic = simulation_model.run_model(geneticOptimization.bestinput, plot_results=True)
 
     return unoptimized_time
 
 
-def run_unoptimized_and_export(input_speed=None, values=None, race_type=Race.FSGP, granularity=1):
+def display_output(return_type, unoptimized, optimized, optimized_random, results, results_random):
+    if return_type is SimulationReturnType.time_taken:
+        print(
+            f'TimeSimulation results. Time Taken: {-1 * unoptimized} seconds, '
+            f'({str(datetime.timedelta(seconds=int(-1 * unoptimized)))})')
+        print(
+            f'Optimized results. Time taken: {-1 * optimized} seconds, '
+            f'({str(datetime.timedelta(seconds=int(-1 * optimized)))})')
+        print(
+            f'Random results. Time taken: {-1 * optimized_random} seconds, '
+            f'({str(datetime.timedelta(seconds=int(-1 * optimized_random)))})')
+
+    elif return_type is SimulationReturnType.distance_travelled:
+        print(f'Distance travelled: {unoptimized}')
+        print(f'Optimized results. Max traversable distance: {optimized}')
+        print(f'Random results. Max traversable distance: {optimized_random}')
+
+    print(f'Optimized Speeds array: {results}')
+    print(f'Random Speeds array: {results_random}')
+
+    return unoptimized
+
+
+def display_commands():
+    """
+
+    Display all valid command line arguments to the user.
+
+    """
+
+    print("------------------------COMMANDS-----------------------\n"
+          "-help                 Display list of valid commands.\n"
+          "\n"
+          "-race_type            Define which race should be simulated. \n"
+          "                      (ASC/FSGP)\n"
+          "\n"
+          "-golang               Define whether golang implementations\n"
+          "                      will be used. \n"
+          "                      (True/False)\n"
+          "\n"
+          "-optimize             Define what data the simulation\n"
+          "                      should optimize. \n"
+          "                      (time_taken/distance_travelled)\n"
+          "\n"
+          "-iter                 Set how many iterations of optimizations\n"
+          "                      should be performed on the simulation.\n"
+          "\n"
+          "-verbose              Set whether simulation methods should\n"
+          "                      execute as verbose.\n"
+          "                      (True/False)\n"
+          "\n"
+          "-route_visualization   Define whether the simulation route\n"
+          "                      should be plotted and visualized.\n"
+          "                      (True/False)\n"
+          "\n"
+          "-granularity          Define how granular the speed array\n"
+          "                      should be, where 1 is hourly and 2 is\n"
+          "                      bi-hourly.\n"
+          "\n"
+          "-------------------------USAGE--------------------------\n"
+          ">>>python3 run_simulation.py -golang=False -optimize=time_taken -iter=3\n")
+
+
+def parse_commands(cmds) -> SimulationSettings:
+    """
+
+    Parse commands from command line into parameters for the simulation.
+
+    :param cmds: list of commands from to be parsed into parameters.
+    :return: return a SimulationParameters object of defaulted or parsed parameters.
+    :rtype: SimulationSettings
+
+    """
+
+    simulation_settings = SimulationSettings()
+
+    command_to_action = {
+        '-golang': lambda x: set_golang(x),
+        '-optimize': lambda x: set_return_type(x),
+        '-iter': lambda x: set_iterations(x),
+        '-verbose': lambda x: set_verbose(x),
+        '-route_visualization': lambda x: set_route_visualization(x),
+        '-race_type': lambda x: set_race_type(x),
+        '-granularity': lambda x: set_granularity(x)
+    }
+
+    def set_golang(value: str):
+        simulation_settings.golang = True if value == 'True' or value == 'true' else False
+
+    def set_return_type(value: str):
+        try:
+            simulation_settings.return_type = SimulationReturnType(value)
+        except ValueError:
+            raise ValueError(f"{value} could not be recognized as a SimulationReturnType!")
+
+    def set_iterations(value: str):
+        simulation_settings.optimization_iterations = int(value)
+
+    def set_verbose(value: str):
+        simulation_settings.verbose = True if value == 'True' or value == 'true' else False
+
+    def set_route_visualization(value: str):
+        simulation_settings.route_visualization = True if value == 'True' or value == 'true' else False
+
+    def set_race_type(value: str):
+        assert value in ['ASC', 'FSGP'], f"Invalid race type {value}. Please enter 'ASC' or 'FSGP'."
+        simulation_settings.race_type = value
+
+    def set_granularity(value: float):
+        simulation_settings.granularity = value
+
+    # If the user has requested '-help', display list of valid commands.
+    if "-help" in cmds:
+        display_commands()
+        exit()
+
+    # Loop through commands and parse them to assign their values to their respective parameters.
+    for cmd in cmds:
+        if not cmd[0] == '-':
+            continue
+
+        split_cmd = cmd.split('=')
+
+        try:
+            action = command_to_action[split_cmd[0]]
+            action(split_cmd[1])
+        except KeyError:
+            raise KeyError(f"{cmd} not identified!")
+
+    return simulation_settings
+
+
+def run_unoptimized_and_export(input_speed=None, values=None, race_type=Race.FSGP, granularity=1, golang=True):
     """
 
     Export simulation data.
@@ -82,6 +262,7 @@ def run_unoptimized_and_export(input_speed=None, values=None, race_type=Race.FSG
     each correspond to a value or array that the Simulation will export. See Simulation.get_results() for valid keys.
     :param race_type: define the race type, either "ASC" or "FSGP"
     :param granularity: define the granularity of Simulation speed array
+    :param golang: define whether GoLang
     implementations should be used.
     :param granularity: control how granular the time divisions of Simulation should be
     :param race_type: whether the race is ASC or FSGP
@@ -89,7 +270,7 @@ def run_unoptimized_and_export(input_speed=None, values=None, race_type=Race.FSG
     """
 
     # Get a basic simulation model
-    simulation_model = build_basic_model(race_type, granularity)
+    simulation_model = build_basic_model(race_type, golang, granularity)
 
     driving_hours = simulation_model.get_driving_time_divisions()
     if input_speed is None:
@@ -99,10 +280,37 @@ def run_unoptimized_and_export(input_speed=None, values=None, race_type=Race.FSG
 
     simulation_model.run_model(speed=input_speed, plot_results=True, verbose=False, route_visualization=False,
                                plot_portion=(0.0 / 8.0, 8.0 / 8.0))
-
     results_array = simulation_model.get_results(values)
 
     return results_array
+
+
+def run_hyperparameter_search(simulation_model: Simulation, bounds: InputBounds):
+    evals_per_setting: int = 3
+    settings_file = results_directory / "settings.csv"
+    stop_index = 0
+
+    with open(settings_file, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        settings_list = GeneticOptimization.parse_csv_into_settings(csv_reader)
+
+    total_num = GeneticOptimization.get_total_generations(settings_list) * evals_per_setting
+    assembler = Assembler(results_directory)
+    with tqdm(total=total_num, file=sys.stdout, desc="Running hyperparameter search", position=0, leave=True) as pbar:
+        try:
+            for settings in settings_list:
+                stop_index += 1
+                for x in range(evals_per_setting):
+                    geneticOptimization = GeneticOptimization(simulation_model, bounds, settings=settings, pbar=pbar,
+                                                              plot_fitness=True)
+                    geneticOptimization.maximize()
+                    evolution = assembler.marshal_evolution(geneticOptimization, simulation_model)
+                    assembler.write_evolution(evolution)
+
+        except KeyboardInterrupt:
+            print(f"Finished {stop_index - 1} setting(s), stopped while evaluating setting {stop_index}.")
+            exit()
+    print("Hyperparameter search has concluded.")
 
 
 def get_default_settings(race_type: Race.RaceType = Race.FSGP) -> tuple[dict, dict]:
@@ -120,11 +328,12 @@ def get_default_settings(race_type: Race.RaceType = Race.FSGP) -> tuple[dict, di
     return initial_conditions, model_parameters
 
 
-def build_basic_model(race_type: Race.RaceType = Race.FSGP, granularity: float = 1) -> Simulation:
+def build_basic_model(race_type: Race.RaceType = Race.FSGP, golang: bool = True, granularity: float = 1) -> Simulation:
     initial_conditions, model_parameters = get_default_settings(race_type)
     simulation_builder = SimulationBuilder() \
         .set_initial_conditions(initial_conditions) \
         .set_model_parameters(model_parameters, Race.RaceType(race_type)) \
+        .set_golang(golang) \
         .set_return_type(SimulationReturnType.void) \
         .set_granularity(granularity)
     return simulation_builder.get()
@@ -165,18 +374,4 @@ def _execute_build_script() -> None:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--race_type", required=False, default="FSGP", help="Define which race should be simulated. ("
-                                                                            "ASC/FSGP)", type=str)
-    parser.add_argument("--granularity", required=False, default=1, help="Define how granular the speed array should "
-                                                                         "be, where 1 is hourly and 2 is bi-hourly.",
-                        type=int)
-    parser.add_argument("-v", "--verbose", required=False, default=False, help="Set to nake simulation execute "
-                                                                               "as verbose.",
-                        action="store_true")
-
-    parser.add_argument('-s', "--speeds", required=False, default=None, help="Name of cached speed array (.npy extension is assumed)", type=str)
-
-    args = parser.parse_args()
-
-    main(SimulationSettings(race_type=args.race_type, verbose=args.verbose, granularity=args.granularity), speeds_filename=args.speeds)
+    main()
--- simulation/config/__init__.py
+++ simulation/config/__init__.py
@@ -1,4 +1,3 @@
 import pathlib
 
 config_directory = pathlib.Path(__file__).parent
-speeds_directory = config_directory / "speeds"
