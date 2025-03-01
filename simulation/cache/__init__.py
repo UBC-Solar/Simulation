285495873195e99f9f9ee4df9697052a70f1f5b8
Joshua Riefman <joshuariefman@gmail.com>
2025-03-01 11:04:21 -0800

    Add cache using shelve

--- simulation/cache/__init__.py
+++ simulation/cache/__init__.py
@@ -1,4 +1,10 @@
 from .cache import (
     query_npz_from_cache,
-    store_npz_to_cache
+    store_npz_to_cache,
+    FSCache,
+    Cache
 )
+
+import pathlib
+
+simulation_cache: Cache = FSCache(pathlib.Path(__file__).parent)
--- simulation/cache/cache.py
+++ simulation/cache/cache.py
@@ -1,10 +1,41 @@
+import pathlib
+import pickle
+
 import numpy as np
 import os
 from pathlib import Path
 from pickle import UnpicklingError
-from typing import Dict, Union, TypeAlias
+from typing import Dict, Union, TypeAlias, Any
+from abc import ABC, abstractmethod
+import shelve
 
 HashLike: TypeAlias = Union[str, list, np.ndarray, int]
+
+
+class Cache(ABC):
+    """
+    An interface to a persistent storage system
+    """
+    @abstractmethod
+    def get(self, file_path: os.PathLike) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def put(self, obj: Any, file_path: os.PathLike):
+        raise NotImplementedError
+
+
+class FSCache(Cache):
+    def __init__(self, shelve_dir: pathlib.Path):
+        self._shelve_dir = shelve_dir
+
+    def get(self, file_path: os.PathLike):
+        with shelve.open(self._shelve_dir, protocol=pickle.HIGHEST_PROTOCOL) as db:
+            return db[file_path]
+
+    def put(self, obj: Any, file_path: os.PathLike):
+        with shelve.open(self._shelve_dir, protocol=pickle.HIGHEST_PROTOCOL) as db:
+            db[file_path] = obj
 
 
 def _hash_equality(hash_1: HashLike, hash_2: HashLike) -> bool:
