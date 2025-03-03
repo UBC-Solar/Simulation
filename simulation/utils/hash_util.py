bbd8e548366cbdb5d70bab16b337786781ecda9e
Joshua Riefman <joshuariefman@gmail.com>
2025-03-02 19:06:40 -0800

    Make hash_dict return an integer

--- simulation/utils/hash_util.py
+++ simulation/utils/hash_util.py
@@ -3,13 +3,13 @@ import json
 from typing import Dict
 
 
-def hash_dict(value: Dict) -> str:
+def hash_dict(value: Dict, num_chars: int = 12) -> int:
     """
     Create a consistent hash for a dictionary by converting it to a string with sorted keys
     and then applying a SHA-256 hash.
     """
     # Convert the data to a JSON string, sorting keys to ensure consistent ordering
-    serialized_data = json.dumps(value, sort_keys=True, separators=(',', ':'))
+    serialized_data = json.dumps(value, sort_keys=True, separators=(',', ':'), default=str)
 
     # Use SHA-256 for consistent and robust hashing
-    return hashlib.sha256(serialized_data.encode('utf-8')).hexdigest()
+    return int(hashlib.sha256(serialized_data.encode('utf-8')).hexdigest()[:num_chars], 16)
