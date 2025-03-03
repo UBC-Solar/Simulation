031bb42807e7590da0b1174fdd201d11aaa0120b
Joshua Riefman <joshuariefman@gmail.com>
2025-03-02 19:04:42 -0800

    Make cache use dill

--- simulation/cache/cache.py
+++ simulation/cache/cache.py
@@ -1,5 +1,6 @@
 import pathlib
 import pickle
+from io import BytesIO
 
 import numpy as np
 import os
@@ -8,8 +9,32 @@ from pickle import UnpicklingError
 from typing import Dict, Union, TypeAlias, Any
 from abc import ABC, abstractmethod
 import shelve
+from dill import Unpickler, Pickler
 
 HashLike: TypeAlias = Union[str, list, np.ndarray, int]
+
+
+class DillShelf(shelve.DbfilenameShelf):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        try:
+            value = self.cache[key]
+        except KeyError:
+            f = BytesIO(self.dict[key.encode(self.keyencoding)])
+            value = Unpickler(f).load()
+            if self.writeback:
+                self.cache[key] = value
+        return value
+
+    def __setitem__(self, key, value):
+        if self.writeback:
+            self.cache[key] = value
+        f = BytesIO()
+        p = Pickler(f, self._protocol)
+        p.dump(value)
+        self.dict[key.encode(self.keyencoding)] = f.getvalue()
 
 
 class Cache(ABC):
@@ -30,12 +55,12 @@ class FSCache(Cache):
         self._shelve_dir = shelve_dir
 
     def get(self, file_path: os.PathLike):
-        with shelve.open(self._shelve_dir, protocol=pickle.HIGHEST_PROTOCOL) as db:
-            return db[file_path]
+        with DillShelf(self._shelve_dir, protocol=pickle.HIGHEST_PROTOCOL) as db:
+            return db[str(file_path)]
 
     def put(self, obj: Any, file_path: os.PathLike):
-        with shelve.open(self._shelve_dir, protocol=pickle.HIGHEST_PROTOCOL) as db:
-            db[file_path] = obj
+        with DillShelf(self._shelve_dir, protocol=pickle.HIGHEST_PROTOCOL) as db:
+            db[str(file_path)] = obj
 
 
 def _hash_equality(hash_1: HashLike, hash_2: HashLike) -> bool:
