b1162ec5518a83d47e39bbf49fa538b881bcc6c1
Joshua Riefman <joshuariefman@gmail.com>
2025-05-28 16:04:02 -0700

    Update config with command settings

--- diagnostic_interface/config.py
+++ diagnostic_interface/config.py
@@ -12,6 +12,37 @@ class Settings(BaseModel):
     sunlink_path: str
 
 
+class CommandSettings(BaseModel):
+    sunbeam_up_cmd: str
+    sunbeam_down_cmd: str
+    sunlink_up_cmd: str
+    sunlink_down_cmd: str
+    telemetry_enable_cmd: str
+
+
+class CommandSettingsProtocol(Protocol):
+    @property
+    def sunbeam_up_cmd(self) -> str: ...
+    @property
+    def sunbeam_down_cmd(self) -> str: ...
+    @property
+    def sunlink_up_cmd(self) -> str: ...
+    @property
+    def sunlink_down_cmd(self) -> str: ...
+    @property
+    def telemetry_enable_cmd(self) -> str: ...
+    @sunbeam_up_cmd.setter
+    def sunbeam_up_cmd(self, value: str): ...
+    @sunbeam_down_cmd.setter
+    def sunbeam_down_cmd(self, value: str): ...
+    @sunlink_up_cmd.setter
+    def sunlink_up_cmd(self, value: str): ...
+    @sunlink_down_cmd.setter
+    def sunlink_down_cmd(self, value: str): ...
+    @telemetry_enable_cmd.setter
+    def telemetry_enable_cmd(self, value: str): ...
+
+
 class SettingsProtocol(Protocol):
     @property
     def plot_timer_interval(self) -> int: ...
@@ -58,4 +89,32 @@ class PersistentSettings:
         self.save()
 
 
+class PersistentCommandSettings:
+    def __init__(self, path: Path = Path(__file__).parent / "commands.toml"):
+        self._path = path
+        self._model = self._load()
+
+    def _load(self) -> CommandSettings:
+        if self._path.exists():
+            with self._path.open("rb") as f:
+                data = tomli.load(f)
+            return CommandSettings(**data)
+        else:
+            return CommandSettings()
+
+    def save(self):
+        with self._path.open("wb") as f:
+            tomli_w.dump(self._model.model_dump(), f)
+
+    def __getattr__(self, item):
+        return getattr(self._model, item)
+
+    def __setattr__(self, key, value):
+        if key in {"_path", "_model"}:
+            return super().__setattr__(key, value)
+        setattr(self._model, key, value)
+        self.save()
+
+
 settings = cast(SettingsProtocol, PersistentSettings())
+command_settings = cast(CommandSettingsProtocol, PersistentCommandSettings())
