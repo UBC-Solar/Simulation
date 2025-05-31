6c9549efd0c61607ea82857d4779cefe3bd6254a
Joshua Riefman <joshuariefman@gmail.com>
2025-05-31 14:28:16 -0700

    Changes for uv + 3.12

--- diagnostic_interface/start_application.py
+++ diagnostic_interface/start_application.py
@@ -3,14 +3,14 @@ import os
 sys.path.insert(0, os.getcwd())
 from PyQt5.QtWidgets import QApplication
 from main_window import MainWindow
-import qdarktheme
+# import qdarktheme
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     # PyQt Theme
-    app.setStyleSheet(qdarktheme.load_stylesheet("light"))
+    # app.setStyleSheet(qdarktheme.load_stylesheet("light"))
 
     window = MainWindow()
     window.showMaximized()
